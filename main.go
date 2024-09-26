37d096621784e228edb5d243f16d37f7ba6cd470
Rishwanth Reddy <rishwanthreddy@Rishwanths-MacBook-Air.local>
2024-09-26 15:07:39 -0400

    created function total

--- main.go
+++ main.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Welcome to Group F's Week 4 Project!")
+	fmt.Println("Welcome to Group F's Week 4 Project!")
+	value := total()
+	fmt.Println(value)
 }
