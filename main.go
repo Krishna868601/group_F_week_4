831749eb3fb14ff5ebd3b51a1e37b39ea84db1ac
Rishwanth Reddy <rishwanthreddy@Rishwanths-MacBook-Air.local>
2024-09-26 16:07:12 -0400

    created function total

--- main.go
+++ main.go
@@ -6,42 +6,29 @@ func printFullName(firstName, lastName string) {
 	fmt.Println("Full Name:", firstName+" "+lastName)
 }
 
-
-
 func max(num1, num2 int) int {
 	var result int
-	if (num1 > num2) {
-	   result = num1
+	if num1 > num2 {
+		result = num1
 	} else {
-	   result = num2
+		result = num2
 	}
-	return result 
- }
-
-
-
-
+	return result
+}
 
 func main() {
 
-	firstName := "Naga"  
+	firstName := "Naga"
 	lastName := "Teja"
 	printFullName(firstName, lastName)
 
-
-
-
-	
 	var a int = 100
-    var b int = 200
-    var c int
-    c = max(a, b)
-    fmt.Printf( "Max value is : %d\n", c )
+	var b int = 200
+	var c int
+	c = max(a, b)
+	fmt.Printf("Max value is : %d\n", c)
 
+	value := total()
+	fmt.Println(value)
 
-	
 }
-
-
-
-
---
+++ total.go
@@ -0,0 +1,9 @@
+package main
+
+func total() int {
+	sum := 0
+	for i := 1; i < 5; i++ {
+		sum += i
+	}
+	return sum
+}
