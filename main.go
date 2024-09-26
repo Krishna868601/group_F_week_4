d5e5ea32a6e1881c3459467b22c4f73949c3d294
PrashanthReddyy18 <mpreddy3636@gmail.com>
2024-09-26 15:57:45 -0400

    added new function maximum of 2 numbers

--- main.go
+++ main.go
@@ -8,6 +8,20 @@ func printFullName(firstName, lastName string) {
 
 
 
+func max(num1, num2 int) int {
+	var result int
+	if (num1 > num2) {
+	   result = num1
+	} else {
+	   result = num2
+	}
+	return result 
+ }
+
+
+
+
+
 func main() {
 
 	firstName := "Naga"  
@@ -15,6 +29,14 @@ func main() {
 	printFullName(firstName, lastName)
 
 
+
+
+	
+	var a int = 100
+    var b int = 200
+    var c int
+    c = max(a, b)
+    fmt.Printf( "Max value is : %d\n", c )
 
 
 	
